7e5a16869775c3e94b51c603815d3e72ae861e9c
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-10-06 18:04:39 +0300

    prettier

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,13 +1,19 @@
-import css from './FeedbackOptions.module.css'
+import css from './FeedbackOptions.module.css';
 
-export const FeedbackOptions = ({options, onLeaveFeedback})=>{
-return(
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
     <div className={css.options}>
-    { options.map((option, index) => (
-        <button className={css.btn} key={option} type="button" name={option} onClick={onLeaveFeedback}>{option}</button>
-    ))
-    }
+      {options.map((option, index) => (
+        <button
+          className={css.btn}
+          key={option}
+          type="button"
+          name={option}
+          onClick={onLeaveFeedback}
+        >
+          {option}
+        </button>
+      ))}
     </div>
-)
-
-}
+  );
+};
